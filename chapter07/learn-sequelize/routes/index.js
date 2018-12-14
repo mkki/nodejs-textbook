f1f213bf1d85d48d56fd6b0965402d3cc06e7014
mkki <gosorn1@gmail.com>
2018-12-15 01:15:33 +0900

    sample code of usage of findAll method using async/await

--- chapter07/learn-sequelize/routes/index.js
+++ chapter07/learn-sequelize/routes/index.js
@@ -4,17 +4,6 @@ var User = require('../models').User;
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  User.findAll()
-    .then((users) => {
-      res.render('sequelize', { users });
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
-});
-
 router.get('/', async (req, ers, next) => {
   try {
     const users = await User.findAll();

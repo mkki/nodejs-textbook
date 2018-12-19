35c18836ef3610c87587de38b20c99f91ea6edcf
mkki <gosorn1@gmail.com>
2018-12-19 13:40:17 +0900

    change to async/wait syntax

--- chapter07/learn-sequelize/routes/comments.js
+++ chapter07/learn-sequelize/routes/comments.js
@@ -3,58 +3,54 @@ var { User, Comment } = require('../models');
 
 var router = express.Router();
 
-router.get('/:id', function(req, res, next) {
-  Comment.findAll({
-    include: {
-      model: User,
-      where: { id: req.params.id },
-    },
-  })
-    .then((comments) => {
-      console.log(comments);
-      res.json(comments);
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
+router.get('/:id', async(req, res, next) => {
+    try {
+        const comments = await Comment.findAll({
+            include: {
+                model: User,
+                where: { id: req.params.id },
+            }
+        });
+        console.log(comments);
+        res.json(comments);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
 });
 
-router.post('/', function(req, res, next) {
-  Comment.create({
-    commenter: req.body.id,
-    comment: req.body.comment,
-  })
-    .then((result) => {
-      console.log(result);
-      res.status(201).json(result);
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
+router.post('/', async(req, res, next) => {
+    try {
+        const result = await Comment.create({
+            commenter: req.body.id,
+            comment: req.body.comment,
+        });
+        console.log(result);
+        res.status(201).json(result);
+    } catch (error) {
+        console.error(err);
+        next(err);
+    }
 });
 
-router.patch('/:id', function(req, res, next) {
-  Comment.update({ comment: req.body.comment }, { where: { id: req.params.id } })
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
+router.patch('/:id', async(req, res, next) => {
+    try {
+        const result = await Comment.update({ comment: req.body.comment }, { where: { id: req.params.id } })
+        res.json(result);
+    } catch (error) {
+        console.error(err);
+        next(err);
+    }
 });
 
-router.delete('/:id', function(req, res, next) {
-  Comment.destroy({ where: { id: req.params.id } })
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
+router.delete('/:id', async(req, res, next) => {
+    try {
+        const result = await Comment.destroy({ where: { id: req.params.id } })
+        res.json(result);
+    } catch (error) {
+        console.error(err);
+        next(err);
+    }
 });
 
 module.exports = router;
--- chapter07/learn-sequelize/routes/users.js
+++ chapter07/learn-sequelize/routes/users.js
@@ -4,31 +4,30 @@ var User = require('../models').User;
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  User.findAll()
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
+router.get('/', async(req, res, next) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (error) {
+        console.error(err);
+        next(err);
+
+    }
 });
 
-router.post('/', function(req, res, next) {
-  User.create({
-    name: req.body.name,
-    age: req.body.age,
-    married: req.body.married,
-  })
-    .then((result) => {
-      console.log(result);
-      res.status(201).json(result);
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
+router.post('/', async(req, res, next) => {
+    try {
+        const result = await User.create({
+            name: req.body.name,
+            age: req.body.age,
+            married: req.body.married,
+        });
+        console.log(result);
+        res.status(201).json(result);
+    } catch (error) {
+        console.error(err);
+        next(err);
+    }
 });
 
 module.exports = router;

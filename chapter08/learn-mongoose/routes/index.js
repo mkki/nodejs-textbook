824adccc58840165fd533eb027e98c5e1908b5c2
mkki <gosorn1@gmail.com>
2018-12-26 18:04:40 +0900

    change to async/await syntax

--- chapter08/learn-mongoose/routes/index.js
+++ chapter08/learn-mongoose/routes/index.js
@@ -4,15 +4,14 @@ var User = require('../schemas/user');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  User.find({})
-    .then((users) => {
-      res.render('mongoose', { users });
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    })
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    res.render('mongoose', { users });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
